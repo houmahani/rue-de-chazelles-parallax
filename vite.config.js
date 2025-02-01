9499033f8103bf9173774625eb1bbb83baf5da00
Houmahani Kane <houmahanikane@gmail.com>
2025-02-01 13:36:25 +0100

    🔧 Updates config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react'
 import glsl from 'vite-plugin-glsl'
 import eslint from 'vite-plugin-eslint'
+import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,14 @@ export default defineConfig({
     glsl({
       include: [/\.glsl$/, /\.vert$/, /\.frag$/],
       defaultExtension: 'glsl',
+    }),
+    viteImagemin({
+      webp: {
+        quality: 75, // Adjust quality (0-100)
+      },
+      pngquant: {
+        quality: [0.6, 0.8], // Compress PNG before converting
+      },
     }),
   ],
   server: {
